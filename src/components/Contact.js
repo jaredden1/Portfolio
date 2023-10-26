e7a3b4c48997d31cd3d9c8fc69fda3b819eba43c
jaredden <james.ifit@gmail.com>
2023-10-26 19:20:25 -0400

    added basic contact form css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
 
   return (
     <>
-      <h2 className="about-subheader">Contact Me</h2>
+      <h2 className="about-subheader">Contact</h2>
       <form ref={form} onSubmit={sendEmail} className="about-contact-form">
         <br />
         <input 
@@ -52,20 +52,6 @@ const Contact = () => {
         ></textarea>
         <button type="submit">Send Message</button>
       </form>
-
-      <ul className="about-developer">
-        James Redden<> | </>
-        <a href="https://www.linkedin.com/in/jamesredden1" className="linkedin">
-          LinkedIn
-        </a>
-        <> | </>
-        <a
-          href="https://github.com/jaredden1/pantry-pal"
-          className="about-github-link"
-        >
-          GitHub
-        </a>
-      </ul>
     </>
   );
 }
